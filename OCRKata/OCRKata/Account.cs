4af13f254bb88e2cb0956334e761e110ea6af9bb
Alan Campbell <alan.vine@outlook.com>
2019-04-23 21:21:48 +0100

    OCR Kata first try, starting again

---
+++ OCRKata/OCRKata/Account.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCRKata
+{
+    public class Account
+    {
+        List<string> _lines = new List<string>();
+        Dictionary<string, int> _digitIndex = new Dictionary<string, int>
+        {
+            { " _ | ||_|", 0},
+            { "     |  |", 1},
+            { " _  _||_ ", 2},
+            { " _  _| _|", 3},
+            { "   |_|  |", 4},
+            { " _ |_  _|", 5},
+            { " _ |_ |_|", 6},
+            { " _   |  |", 7},
+            { " _ |_||_|", 8},
+            { " _ |_| _|", 9}
+        };
+        
+        public void AddLine(string line)
+        {
+            _lines.Add(line);
+        }
+
+        public bool IsValidAccountText
+        {
+            get
+            {
+                return
+                    (_lines.Where(a => a.Length == 27).Count() == 3)
+                    && (_lines.SelectMany(a => a.ToCharArray())
+                            .Where(b => b != '_' && b != '|').Count() == 0);
+            }
+        }
+
+        public string Number
+        {
+            get
+            {
+                var digits = new List<string>()
+                {
+                    "", "", "", "", "", "", "", "", ""
+                };
+
+                foreach (var line in _lines)
+                {
+                    for (int i = 0; i < 9; i += 1)
+                    {
+                        var builder = new StringBuilder(digits[i]);
+                        digits[i] = builder.Append(line.Substring(i * 3, 3)).ToString();
+                    }
+                }
+                return getNumbersFromDigits(digits);
+            }
+        }
+
+        private string getNumbersFromDigits(List<string> digits)
+        {
+            var result = new StringBuilder();
+            for(int i=0; i<9; i++)
+            {
+                result.Append(getNumber(digits[i]));
+            }
+            return result.ToString();
+        }
+
+        private string getNumber(string digit)
+        {
+            if(_digitIndex.ContainsKey(digit))
+            {
+                return _digitIndex[digit].ToString();
+            }
+            return "?";
+        }
+
+    }
+}
---
+++ OCRKata/OCRKata/OCRTest.cs
@@ -0,0 +1,158 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCRKata
+{
+    [TestFixture]
+    public class OCRTest
+    {
+        Account _account;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _account = new Account();
+        }
+
+        [Test]
+        public void AccountTextWith3LinesAnd27CharactersInEachLineReturnsValid()
+        {
+            _account.AddLine("_________|_________|_______");
+            _account.AddLine("_________|_________|_______");
+            _account.AddLine("_________|_________|_______");
+            Assert.IsTrue(_account.IsValidAccountText);
+        }
+
+        [Test]
+        public void AccountTextWithNot3LinesReturnsInvalid()
+        {
+            _account.AddLine("_________|_________|_______");
+            _account.AddLine("_________|_________|_______");
+            Assert.IsFalse(_account.IsValidAccountText);
+        }
+
+        [Test]
+        public void AccountTextWith3LinesButNot27CharactersInEachLineReturnsInValid()
+        {
+            _account.AddLine("_________|_________|_______");
+            _account.AddLine("_________|_______|_______");
+            _account.AddLine("_________|_________|_______");
+            Assert.IsFalse(_account.IsValidAccountText);
+        }
+
+        [Test]
+        public void AccountTextWithInvalidCharactersReturnsInvalid()
+        {
+            _account.AddLine("_________|_________|_______");
+            _account.AddLine("_________|___g_____|_______");
+            _account.AddLine("_________|_________|_______");
+            Assert.IsFalse(_account.IsValidAccountText);
+        }
+
+        [Test]
+        public void ReturnsAllZeros()
+        {
+            _account.AddLine(" _  _  _  _  _  _  _  _  _ ");
+            _account.AddLine("| || || || || || || || || |");
+            _account.AddLine("|_||_||_||_||_||_||_||_||_|");
+            Assert.AreEqual("000000000", _account.Number);
+        }
+
+        [Test]
+        public void ReturnsAllOnes()
+        {
+            _account.AddLine("                           ");
+            _account.AddLine("  |  |  |  |  |  |  |  |  |");
+            _account.AddLine("  |  |  |  |  |  |  |  |  |");
+            Assert.AreEqual("111111111", _account.Number);
+        }
+
+        [Test]
+        public void ReturnsAllTwos()
+        {
+            _account.AddLine(" _  _  _  _  _  _  _  _  _ ");
+            _account.AddLine(" _| _| _| _| _| _| _| _| _|");
+            _account.AddLine("|_ |_ |_ |_ |_ |_ |_ |_ |_ ");
+            Assert.AreEqual("222222222", _account.Number);
+        }
+
+        [Test]
+        public void ReturnsAllThrees()
+        {
+            _account.AddLine(" _  _  _  _  _  _  _  _  _ ");
+            _account.AddLine(" _| _| _| _| _| _| _| _| _|");
+            _account.AddLine(" _| _| _| _| _| _| _| _| _|");
+            Assert.AreEqual("333333333", _account.Number);
+        }
+
+        [Test]
+        public void ReturnsAllFours()
+        {
+            _account.AddLine("                           ");
+            _account.AddLine("|_||_||_||_||_||_||_||_||_|");
+            _account.AddLine("  |  |  |  |  |  |  |  |  |");
+            Assert.AreEqual("444444444", _account.Number);
+        }
+
+        [Test]
+        public void ReturnsAllFives()
+        {
+            _account.AddLine(" _  _  _  _  _  _  _  _  _ ");
+            _account.AddLine("|_ |_ |_ |_ |_ |_ |_ |_ |_ ");
+            _account.AddLine(" _| _| _| _| _| _| _| _| _|");
+            Assert.AreEqual("555555555", _account.Number);
+        }
+
+        [Test]
+        public void ReturnsAllSixes()
+        {
+            _account.AddLine(" _  _  _  _  _  _  _  _  _ ");
+            _account.AddLine("|_ |_ |_ |_ |_ |_ |_ |_ |_ ");
+            _account.AddLine("|_||_||_||_||_||_||_||_||_|");
+            Assert.AreEqual("666666666", _account.Number);
+        }
+
+        [Test]
+        public void ReturnsAllSevens()
+        {
+            _account.AddLine(" _  _  _  _  _  _  _  _  _ ");
+            _account.AddLine("  |  |  |  |  |  |  |  |  |");
+            _account.AddLine("  |  |  |  |  |  |  |  |  |");
+            Assert.AreEqual("777777777", _account.Number);
+        }
+
+        [Test]
+        public void ReturnsAllEights()
+        {
+            _account.AddLine(" _  _  _  _  _  _  _  _  _ ");
+            _account.AddLine("|_||_||_||_||_||_||_||_||_|");
+            _account.AddLine("|_||_||_||_||_||_||_||_||_|");
+            Assert.AreEqual("888888888", _account.Number);
+        }
+
+        [Test]
+        public void ReturnsAllNines()
+        {
+            _account.AddLine(" _  _  _  _  _  _  _  _  _ ");
+            _account.AddLine("|_||_||_||_||_||_||_||_||_|");
+            _account.AddLine(" _| _| _| _| _| _| _| _| _|");
+            Assert.AreEqual("999999999", _account.Number);
+        }
+
+        [Test]
+        public void ReturnsAllNumbers()
+        {
+            _account.AddLine("    _  _     _  _  _  _  _ ");
+            _account.AddLine("  | _| _||_||_ |_   ||_||_|");
+            _account.AddLine("  ||_  _|  | _||_|  ||_| _|");
+            Assert.AreEqual("123456789", _account.Number);
+        }
+
+        private string digits[] = new string
+
+    }
+}
